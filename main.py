4f1225a1fdb0284557120a6532fafdfb05f6f42f
Asmar Sajjad <amsarsajjad3@mail.com>
2025-12-06 16:57:17 +0000

    Implemented Max Pooling

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 from imatrix import load_img
 from imkernel import *
 from draw import draw_matrix
+from pool import max_pool
 
 if len(sys.argv) < 2:
     print("Usage: python main.py filename.png")
@@ -9,7 +10,18 @@ if len(sys.argv) < 2:
 
 filename = sys.argv[1]
 
-# load image, apply kernel adn draw the processed image
+# load image
 matrix = load_img(filename)
-new = apply_kernel(matrix, edge)
-draw_matrix(new, pixel_size=1, title="Processed image")
+
+# CNN LAYER 1
+
+# apply edge kernel
+layer1 = apply_kernel(matrix, edge)
+# apply the pooling to the new image
+layer1 = max_pool(layer1)
+
+# apply edge kernel again
+layer1 = apply_kernel(layer1, edge)
+
+
+draw_matrix(layer1, pixel_size=1, title="Processed image")
---
+++ pool.py
@@ -0,0 +1,41 @@
+# The pooling section will be 2x2 
+def max_pool(matrix, pool_size=2):
+    pooled_vals = []
+
+    for i in range(0, len(matrix), pool_size):
+        for j in range(0, len(matrix[0]), pool_size):
+            section = [
+                matrix[i][j], matrix[i][j+1],
+                matrix[i+1][j], matrix[i+1][j+1]
+            ]
+
+            # Use max pooling to get the max value
+            max_val = max(section)
+
+            pooled_vals.append(max_val)
+
+    # reconstuct the pooled values into a matrix
+    new_matrix = []
+    count = 0
+    for _ in range(len(matrix)//pool_size):
+        row = []
+        for _ in range(len(matrix[0])//pool_size):
+            row.append(pooled_vals[count])
+            count += 1
+
+        new_matrix.append(row)
+
+    return new_matrix
+
+if __name__ == "__main__":
+    test_matrix = [
+        [1,2,3,4,5,6],
+        [7,8,9,10,11,12],
+        [13,14,15,16,17,18],
+        [19,20,21,22,23,24],
+        [25,26,27,28,29,30],
+        [31,32,33,34,35,36]
+    ]
+
+
+    max_pool(test_matrix)
