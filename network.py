cbaaa941d8c2483b886257e654a5fd127aabd0b7
Asmar Sajjad <amsarsajjad3@mail.com>
2026-04-10 19:04:34 +0100

    Added back propogation

--- network.py
+++ network.py
@@ -37,7 +37,7 @@ def softmax(scores):
     # here we use this variable to make sure that we dont reach errors when the score is large
 
     # find the e^score for all scores
-    exp_scores = [m.exp[score-max_score] for score in scores]
+    exp_scores = [m.exp(score-max_score) for score in scores]
 
     # find the sum of all the exponent - this is the demonator for all the probability calculations
     sum_exp = sum(exp_scores)
@@ -46,4 +46,33 @@ def softmax(scores):
     probs = [(score/sum_exp) for score in exp_scores]
 
     return probs
+
+
+def calculate_error(probs, actual_label):
+    # use -ln(prob of correct digit)
+    # add 1e-10 to prevent a maths error if prob is 0
+    return -m.log(probs[actual_label] + 1e-10)
+
+# now we work backwards to find errors and correct the weight and biases
+
+def backward_propogate(flat_list, probs, actual_label):
+    # clauclate the error for each node (dz) - this is predcition - actual value
+    dz = []
+    for i in range(len(probs)):
+        target = 1.0 if i == actual_label else 0.0
+        # find the difference in probaility between if the model was 100% correct, and he current model
+        dz.append(probs[i] - target)
+
+    # caulcate the weight gradients (dW)
+    # dW = error * input_pixel
+    dW  = []
+    for error in dz:
+        # If the pixel was bright (1.0), it gets more 'blame' for the error
+        row_gradients = [error * pixel for pixel in flat_list]
+        dW.append(row_gradients)
+
+    # the bias gradients (dB) are just the errors themselves
+    dB = dz
+
+    return dW, dB
 
